fe00767b077e2f2cdacd384393537d171d482c38
Junaid Khan <127403229+itsMeJunaid@users.noreply.github.com>
2024-03-22 20:38:47 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["Machine Learning", "AI development", "Chatbots Development", "Front-End Development", "Data Scrapping", "Data Minging"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
